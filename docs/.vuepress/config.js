abff5750871e1c19fe0471d42d6025a48c045252
Nature-log <3214228964@qq.com>
2020-11-14 18:27:59 +0800

    vuepress init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    themeConfig: {
+        title: './public/logoko.png',
+        navbar: true,
+        logo: './img/logo.png',
+        nav: [    //导航栏配置
+
+            { text: '主页', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ]
+    }
+}
